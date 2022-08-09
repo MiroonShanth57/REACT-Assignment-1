a1cf2347558ef196e7599c8ecef3791c93190a07
MSHANTH <miroonshanth123@gmail.com>
2022-08-09 19:06:14 +0530

    Updated Home Page

--- src/components/DashBoard/index.jsx
+++ src/components/DashBoard/index.jsx
@@ -1,8 +1,17 @@
 import { Component, useState } from "react";
 import * as React from 'react';
-import { Avatar, Grid, Paper, Tabs, Toolbar, Typography, Tab} from "@mui/material";
+import { Avatar, Paper, Tabs, Toolbar, Typography, Tab} from "@mui/material";
 import AppBar from '@mui/material/AppBar';
 import FlutterDashIcon from '@mui/icons-material/FlutterDash';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import WidgetsIcon from '@mui/icons-material/Widgets';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import PeopleIcon from '@mui/icons-material/People';
+import Grid from '@mui/material/Unstable_Grid2';
+import PlaylistAddCheckCircleIcon from '@mui/icons-material/PlaylistAddCheckCircle';
+
 
 
 
@@ -15,12 +24,15 @@ class HomeDashBoard extends Component{
 
     render(){
 
-        
-
+        const paperStyleForHome={padding:70, height:'50vh', width:1000, margin:"80px auto"}
+        const styleForCar={ height:200, width:200}
+        const styleForHedingCard={alignItems: "center"}
         // const [value, setValue]=useState();
 
         return(
-            <React.Fragment>
+
+            <Grid>
+                <React.Fragment>
                 <AppBar sx={{background:"gray"}}>
                     <Toolbar>
                         <Avatar sx={{background:"blue"}}>
@@ -51,6 +63,81 @@ class HomeDashBoard extends Component{
                     
                 </AppBar>
             </React.Fragment>
+
+
+            <Grid style={paperStyleForHome} container spacing={1}>
+                
+                <Grid item xs={4}>
+                    <Card sx={{ minWidth: 100}} style={styleForCar}>
+                        <CardContent>
+                            <Typography variant="h5">
+                                Products
+                            </Typography>
+
+                            <br/>
+                            <Avatar>
+                                <WidgetsIcon/>
+                            </Avatar>
+
+                            <Typography variant="h2">
+                                00
+                            </Typography>
+
+                        </CardContent>
+                
+                    </Card>
+                </Grid>
+
+                <Grid item xs={4}>
+                    <Card sx={{ minWidth: 100 }} style={styleForCar}>
+                        <CardContent>
+                            <Typography variant="h5">
+                                Cart
+                            </Typography>
+
+                            <br/>
+                            <Avatar>
+                                <ShoppingCartIcon/>
+                            </Avatar>
+
+                            <Typography variant="h2">
+                                00
+                            </Typography>
+
+                        </CardContent>
+                
+                    </Card>
+                </Grid>
+
+                <Grid item xs={4}>
+                    <Card sx={{ minWidth: 100 }} style={styleForCar}>
+                        <CardContent>
+                            <Typography variant="h5">
+                                User
+                            </Typography>
+
+                        <br/>
+                            <Avatar>
+                                <PeopleIcon/>
+                            </Avatar>
+
+                            <Typography variant="h2">
+                                00
+                            </Typography>
+
+                        </CardContent>
+                
+                    </Card>
+                </Grid>
+
+
+                
+
+            </Grid>
+
+            </Grid>
+            
+            
         )
     }
 }
