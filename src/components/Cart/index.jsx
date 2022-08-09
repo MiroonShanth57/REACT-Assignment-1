95c6fe47cda1feb9ce11b4a5ed4ab06875846a8b
MSHANTH <miroonshanth123@gmail.com>
2022-08-09 21:55:26 +0530

    Updated

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,5 +1,11 @@
 import { Component } from "react";
 import * as React from 'react';
+import { Avatar, Grid, Paper, Typography } from "@mui/material";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import CreateIcon from '@mui/icons-material/Create';
+import CancelIcon from '@mui/icons-material/Cancel';
+
 
 
 class CartMange extends Component{
@@ -9,8 +15,39 @@ class CartMange extends Component{
     }
 
     render(){
+
+        const paperStyleForCart={padding:40, height:'40vh', width:550 , margin:"20px auto"}
+        const TextFieldForForCart={margin:'10px 5px',width:250}
+        const btnstyleForRe={margin:'10px 5px', width:200}
+
+
         return(
-            <h1>Hello</h1>
+            <Grid>
+                <Paper style={paperStyleForCart}>
+                    <Grid>
+                        <Typography variant="h6">
+                            Your Shopping Cart
+                        </Typography>
+                    </Grid>
+                        <br/>
+                    <TextField id="filled-basic" label="User Name" variant="filled" size="small"  placeholder="Select Your User Name Here" required select style={TextFieldForForCart}/>
+                    <TextField id="filled-basic" label="Date" variant="filled" size="small" required style={TextFieldForForCart} Date/>
+
+                    <TextField id="filled-basic" label="Product Type" variant="filled" size="small"  placeholder="Select Your User Name Here" required select style={TextFieldForForCart}/>
+                    <TextField id="filled-basic" label="QTY" variant="filled" size="small"  placeholder="Select Your User Name Here" required input style={TextFieldForForCart}/>
+
+                    
+                    <Button variant="outlined" endIcon={<CreateIcon />} color="success"  style={ btnstyleForRe }>
+                            Confirme
+                        </Button>
+
+                        <Button variant="outlined" endIcon={<CancelIcon />} color="error"  style={ btnstyleForRe }>
+                            Cancel
+                        </Button>
+
+
+                </Paper>
+            </Grid>
         )
     }
 }
--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -1,5 +1,10 @@
 import { Component } from "react";
 import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import CreateIcon from '@mui/icons-material/Create';
+import CancelIcon from '@mui/icons-material/Cancel';
+import { Avatar, Grid, Paper, Typography } from "@mui/material";
 
 
 
@@ -10,10 +15,36 @@ class ProductsMange extends Component{
     }
 
     render(){
+        const paperStyleForCart={padding:40, height:'40vh', width:550 , margin:"20px auto"}
+        const TextFieldForForCart={margin:'10px 5px',width:250}
+        const btnstyleForRe={margin:'10px 5px', width:200}
         return(
-            <div>
-                <h1>hiii</h1>
-            </div>
+            <Grid>
+                <Paper style={paperStyleForCart}>
+                    <Grid>
+                        <Typography variant="h6">
+                            Product Manage
+                        </Typography>
+                    </Grid>
+                        <br/>
+                    <TextField id="filled-basic" label="Time" variant="filled" size="small"  placeholder="Select Your User Name Here" required style={TextFieldForForCart}/>
+                    <TextField id="filled-basic" label="Price" variant="filled" size="small" required style={TextFieldForForCart} Date/>
+
+                    <TextField id="filled-basic" label="Category" variant="filled" size="small"  placeholder="Select Your User Name Here" required select style={TextFieldForForCart}/>
+                    <TextField id="filled-basic" label="Description" variant="filled" size="small"  placeholder="Select Your User Name Here" required input style={TextFieldForForCart}/>
+
+                    
+                    <Button variant="outlined" endIcon={<CreateIcon />} color="success"  style={ btnstyleForRe }>
+                            confirmed
+                        </Button>
+
+                        <Button variant="outlined" endIcon={<CancelIcon />} color="error"  style={ btnstyleForRe }>
+                            Cancel
+                        </Button>
+
+
+                </Paper>
+            </Grid>
         )
 
     }
